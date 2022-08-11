4fed986574e538965d9802c04681876c3617fdf0
Fadi Alladkani <fadialladkani@gmail.com>
2022-08-11 17:56:46 -0400

    Added custom html exporter and styling to match

--- org-publish-config.el
+++ org-publish-config.el
@@ -1,5 +1,83 @@
-;; Enable org-mode
+;; Enable exporter and org-mode
+(require 'ox)
 (require 'org)
+
+;; Disable adding the validation link
+(setq org-html-validation-link nil)
+;; Disable using underscores to make a word into a subscript
+(setq org-export-with-sub-superscripts nil)
+
+;; Since I don't know how to use org/elisp's timestamp conversion functions
+;; And I get the month as an integer, these will map the month number to the actual name
+(setq month-names '("January" "February" "March" "April" "May" "June" "July"
+		    "August" "September" "October" "November" "December"))
+
+;; Site title
+(setq site-title "Org Sync Files")
+
+;; Custom exporter functions
+(defun pico-html-template (contents info)
+  (concat
+   "<!DOCTYPE html>\n"
+   (format "<html lang=\"%s\">\n" (plist-get info :language))
+   "<head>\n"
+   (format "<meta charset=\"%s\">\n"
+	   (coding-system-get org-html-coding-system 'mime-charset))
+   (format "<title>%s</title>\n"
+	   (org-export-data (or (plist-get info :title) "") info))
+   (format "<meta name=\"author\" content=\"%s\">\n"
+	   (org-export-data (plist-get info :author) info))
+   "<link href=\"css/pico.min.css\" rel=\"stylesheet\" style=\"text/css\" />\n"
+   "<script src=\"https://orgmode.org/org-info.js\"></script>"
+   "</head>\n"
+   "<body>\n"
+   "<main class=\"container\">"
+   "<hgroup>"
+   (format "<h1><a href=\"/index.html\">%s</a></h1>" site-title)
+   "<h2>"
+   (format "%s"
+	   (file-name-base (plist-get info :input-file)))
+   "</h2>"
+   "</hgroup>"
+   contents
+   "</main>"
+   "</body>\n"
+   "</html>\n"))
+
+(defun format-timestamp (timestamp)
+  (format "%s %s, %s %s:%s"
+	  (nth (- (org-element-property :month-start timestamp) 1) month-names)
+	  (org-element-property :day-start timestamp)
+	  (org-element-property :year-start timestamp)
+	  (org-element-property :hour-start timestamp)
+	  (org-element-property :minute-start timestamp)))
+
+(defun pico-html-headline (object contents info)
+  (concat
+   "<details><summary>"
+   (if (string= "TODO" (org-element-property :todo-keyword object))
+       "<ins><strong>TODO </strong></ins>" "")
+   (if (string= "DONE" (org-element-property :todo-keyword object))
+       (format "<mark><em data-tooltip=\"%s\">DONE </em></mark>"
+	       (format-timestamp (org-element-property :closed object))))
+   (format "%s</summary>"
+	   (nth 0 (org-element-property :title object)))
+   "<blockquote>"
+   contents
+   "</blockquote>"
+   "</details>\n"))
+
+;; Custom exporter
+(org-export-define-derived-backend 'pico-html 'html
+				   :translate-alist '((template . pico-html-template)
+						      (headline . pico-html-headline)))
+
+(setq org-html-toplevel-hlevel 5)
+(setq org-export-with-toc nil)
+
+;; Publisher with the custom exporter
+(defun org-html-publish-to-pico-html (plist filename pub-dir)
+  (org-publish-org-to 'pico-html filename ".html" plist pub-dir))
 
 ;; Declare primary project
 (setq org-publish-project-alist
@@ -9,7 +87,13 @@
 	 :recursive 1
 	 :auto-sitemap 1
 	 :sitemap-filename "index.html"
-	 :publishing-function org-html-publish-to-html)))
+	 :sitemap-title "Org-Sync Files"
+	 :publishing-function org-html-publish-to-pico-html)
+	("css"
+	 :base-directory "./styling/pico/css"
+	 :base-extension "css"
+	 :publishing-directory "./out/css"
+	 :publishing-function org-publish-attachment)))
 
 ;; Publish all projects to the publishing directory
-(org-publish-all)
+(org-publish-all t)
