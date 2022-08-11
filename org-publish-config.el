dd86c3875b0740b4b658aa146027e3d485eacfbf
Fadi Alladkani <fadialladkani@gmail.com>
2022-08-11 11:08:43 -0400

    Added basic config and workflow

---
+++ org-publish-config.el
@@ -0,0 +1,13 @@
+#!/usr/bin/emacs --script
+
+;; Declare primary project
+(setq org-publish-project-alist
+      '(("primary"
+	 :base-directory "./"
+	 :publishing-directory "./out"
+	 :recursive 1
+	 :auto-sitemap 1
+	 :publishing-function org-html-publish-to-html)))
+
+;; Publish all projects to the publishing directory
+(org-publish-all)
